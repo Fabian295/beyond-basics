f4f60b86c16d32fc5a3b02b6b54c92e7b7b98ef7
Fabian295 <fabiankanhai70@gmail.com>
2022-11-18 16:25:03 +0100

    Add declaration class App and  basic initiation  test

--- docs/gh_p-docs.js
+++ docs/gh_p-docs.js
@@ -1,1 +1,13 @@
 console.log('From the docs folder')
+
+//SPA with vanilla JS
+class App {
+  constructor() {}
+   init = () => {
+    console.log('run the class App!')
+   }
+}
+
+const app = new App()
+
+document.addEventListener('DOMContentLoaded', app.init);
